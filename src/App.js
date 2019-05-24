dae3bed87b3313bf2443fced8e8292c4d19bfd06
Furkan Çelik <furkancelik@Furkan-MacBook-Pro.local>
2019-05-24 15:51:06 +0300

    task is commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import {
+  faStroopwafel,
+  faBars,
+  faComments,
+  faMapMarker
+} from "@fortawesome/free-solid-svg-icons";
+import Header from "./components/Header";
+import Slide from "./components/Slide";
+import DateBoxItem from "./components/DateBoxItem";
+import Content from "./components/Content";
+
+library.add(faStroopwafel, faBars, faComments, faMapMarker);
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+      <Slide />
+      <div style={{ display: "flex" }}>
+        {[
+          { title: "MON", day: 26 },
+          { title: "TUE", day: 27 },
+          { title: "WED", day: 28 },
+          { title: "THU", day: 29 },
+          { title: "FRI", day: 30 }
+        ].map((item, index) => (
+          <DateBoxItem active={index === 0} item={item} />
+        ))}
+      </div>
+      <Content />
+    </>
   );
 }
 
